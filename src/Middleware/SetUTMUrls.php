10d6cef47eb298820708267c297632e302e6a01e
Robin Dirksen <robin@famdirksen.nl>
2018-07-26 21:01:04 +0200

    check if a string contains a hashtag

--- src/Middleware/SetUTMUrls.php
+++ src/Middleware/SetUTMUrls.php
@@ -59,29 +59,34 @@ class SetUTMUrls
             if (isset($aHrefLink['host']) && ! empty($aHrefLink['host']) && in_array($aHrefLink['host'], config('utm-urls.skipped_hosts', []))) {
                 return $match[0];
             } else {
-                //check if there is already an ? in the url
-                if (strpos($url, '?') === false) {
-                    $counter = 0;
+                //check if there is a # in the url
+                if (strpos($url, '#') === false) {
+                    return $match[0];
                 } else {
-                    $counter = 1;
-                }
-
-                //set all the utms
-                foreach (config('utm-urls.utms', []) as $utm => $value) {
-                    if (! empty($value)) {
-                        if ($counter == 0) {
-                            $url .= '?';
-                        } else {
-                            $url .= '&';
-                        }
-
-                        $url .= 'utm_'.str_slug(strtolower($utm)).'='.$value;
-
-                        $counter++;
+                    //check if there is already an ? in the url
+                    if (strpos($url, '?') === false) {
+                        $counter = 0;
+                    } else {
+                        $counter = 1;
                     }
-                }
 
-                return $match[1].$url.$match[3];
+                    //set all the utms
+                    foreach (config('utm-urls.utms', []) as $utm => $value) {
+                        if (!empty($value)) {
+                            if ($counter == 0) {
+                                $url .= '?';
+                            } else {
+                                $url .= '&';
+                            }
+
+                            $url .= 'utm_' . str_slug(strtolower($utm)) . '=' . $value;
+
+                            $counter++;
+                        }
+                    }
+
+                    return $match[1] . $url . $match[3];
+                }
             }
         }, $string);
     }
